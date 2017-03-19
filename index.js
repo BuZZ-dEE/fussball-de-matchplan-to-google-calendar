72b90e20c56fb50a9cefa26a41ca008af681bc9e
Sebastian Schlatow <deezzub@gmail.com>
2017-03-20 00:50:03 +0100

    Initial implementation for Google calendar inserting

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 
 var commander = require('commander');
-var jsdom = require('jsdom');
 var path = require('path');
-var pkg = require( path.join(__dirname, 'package.json') );
+var pkg = require(path.join(__dirname, 'package.json'));
+var fs = require('fs');
+var fussballDeMatchplanGrabber = require('fussball-de-matchplan-grabber');
+var readline = require('readline');
+var googleAuth = require('google-auth-library');
+var google = require('googleapis');
+var getTimezone = require('node-timezone').getTimezone
+var moment = require('moment');
 var yamlConfig = require('node-yaml-config');
 
 
-/** @type String[] */
-var matchPlanArr = [];
-/** @type Matchplan */
-var matchPlan;
-/** @type String[] */
-var teamVsTeam = [];
 
 commander.version(pkg.version)
 	.option('-t, --team <team>', 'The team url.')
@@ -21,155 +21,173 @@ commander.version(pkg.version)
 var config = yamlConfig.load(__dirname + '/config.yml');
 var team = commander.team || config.url.next_games + config.team.id;
 
-jsdom.env({
-  url: team,
-  scripts: ["http://code.jquery.com/jquery.js"],
-  done: function (err, window) {
-    var $ = window.$;
+fussballDeMatchplanGrabber.parseMatchplan(team, googleCalendarImport);
 
-    $("tr.row-headline").each(function() {
-        matchPlanArr.push(parseMatchTime($(this).text()))
+// If modifying these scopes, delete your previously saved credentials
+// at ~/.credentials/calendar-nodejs-quickstart.json
+// var SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+var SCOPES = ['https://www.googleapis.com/auth/calendar'];
+var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+    process.env.USERPROFILE) + '/.credentials/';
+var TOKEN_PATH = TOKEN_DIR + 'calendar-nodejs-quickstart.json';
+
+function googleCalendarImport(plan) {
+    matchPlan = plan;
+    // Load client secrets from a local file.
+    fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+        if (err) {
+            console.log('Error loading client secret file: ' + err);
+            return;
+        }
+        // Authorize a client with the loaded credentials, then call the
+        // Google Calendar API.
+        authorize(JSON.parse(content), insertEvents);
     });
-
-    $(".club-name").each(function() {
-        teamVsTeam.push($(this).text());
-    });
-
-    createMatchplan();
-    outputMatchplan();
-  }
-});
+}
 
 /**
- * Parse the match time.
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
  *
- * @param {String} matchTime
- * @returns {Date} The match date time.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
  */
-function parseMatchTime(matchTime) {
+function authorize(credentials, callback) {
+    var clientSecret = credentials.installed.client_secret;
+    var clientId = credentials.installed.client_id;
+    var redirectUrl = credentials.installed.redirect_uris[0];
+    var auth = new googleAuth();
+    var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
 
-    /** @type String[] */
-    var matchTimeArr = matchTime.split(' ');
-    matchTimeArr = [matchTimeArr[1], matchTimeArr[3]];
-    var dayMonthYear = matchTimeArr[0].split('.');
-    var hourMinutes = matchTimeArr[1].split(':');
-    var date = new Date(dayMonthYear[2], parseInt(dayMonthYear[1]) - 1, dayMonthYear[0], hourMinutes[0], hourMinutes[1]);
-
-    return date;
-}
-
-/**
- * Create the match plan.
- */
-function createMatchplan() {
-    var offset = 0;
-    matchPlan = new Matchplan();
-    matchPlanArr.forEach(function(currentValue, index, array) {
-        matchplanEntry = new MatchplanEntry(currentValue, null, teamVsTeam[offset], teamVsTeam[++offset]);
-        matchPlan.addEntry(matchplanEntry);
-        offset++;
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, function (err, token) {
+        if (err) {
+            getNewToken(oauth2Client, callback);
+        } else {
+            oauth2Client.credentials = JSON.parse(token);
+            callback(oauth2Client);
+        }
     });
 }
 
 /**
- * Console output of the match plan.
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ *
+ * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback to call with the authorized
+ *     client.
  */
-function outputMatchplan() {
-    matchPlan.forEach(function(currentValue, index, array) {
-        console.log(currentValue.toString());
+function getNewToken(oauth2Client, callback) {
+    var authUrl = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES
+    });
+    console.log('Authorize this app by visiting this url: ', authUrl);
+    var rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.question('Enter the code from that page here: ', function (code) {
+        rl.close();
+        oauth2Client.getToken(code, function (err, token) {
+            if (err) {
+                console.log('Error while trying to retrieve access token', err);
+                return;
+            }
+            oauth2Client.credentials = token;
+            storeToken(token);
+            callback(oauth2Client);
+        });
     });
 }
 
-var Matchplan = (function() {
-    /**
-     * @constructor
-     */
-    function Matchplan() { };
-
-    Matchplan.prototype = new Array;
-    Matchplan.prototype.addEntry = Matchplan.prototype.push;
-
-    return Matchplan;
-})();
+/**
+ * Store token to disk be used in later program executions.
+ *
+ * @param {Object} token The token to store to disk.
+ */
+function storeToken(token) {
+    try {
+        fs.mkdirSync(TOKEN_DIR);
+    } catch (err) {
+        if (err.code != 'EEXIST') {
+            throw err;
+        }
+    }
+    fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+    console.log('Token stored to ' + TOKEN_PATH);
+}
 
 /**
- * @typedef {MatchplanEntry}
+ * Lists the next 10 events on the user's primary calendar.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
-var MatchplanEntry = (function() {
-    /**
-     * @constructor
-     * @param {String} date - The date time of the match.
-     * @param {String} location - The location where the game takes place.
-     * @param {String} hometeam - The name of the home team.
-     * @param {String} visitingteam - The name of the visiting team.
-     * @param {String} description - A match description.
-     * @param {String} url - An URL to match information.
-     */
-    function MatchplanEntry(date, location, hometeam, visitingteam, description, url) {
-        this.date = date;
-        this.location = location;
-        this.hometeam = hometeam;
-        this.visitingteam = visitingteam;
-        this.description = description;
-        this.url = url;
-    };
+function listEvents(auth) {
+    var calendar = google.calendar('v3');
+    calendar.events.list({
+        auth: auth,
+        calendarId: '4ag87iqhp7nr3u67ncriuvn0js@group.calendar.google.com', //'GVO4test',
+        timeMin: (new Date()).toISOString(),
+        maxResults: 10,
+        singleEvents: true,
+        orderBy: 'startTime'
+    }, function (err, response) {
+        if (err) {
+            console.log('The API returned an error: ' + err);
+            return;
+        }
+        var events = response.items;
+        if (events.length == 0) {
+            console.log('No upcoming events found.');
+        } else {
+            console.log('Upcoming 10 events:');
+            for (var i = 0; i < events.length; i++) {
+                var event = events[i];
+                var start = event.start.dateTime || event.start.date;
+                console.log('%s - %s', start, event.summary);
+            }
+        }
+    });
+}
 
-    MatchplanEntry.prototype.getDate = function() {
-        return this.date;
-    };
+function insertEvents(auth) {
+    matchPlan.forEach(function (value, index, array) {
+        var googleCalendarEvent = {
+            'summary': 'Spiel: ' + value.getHomeTeam() + ' vs ' + value.getVisitingTeam(),
+            'location': value.getLocation(),
+            'description': value.getDescription(),
+            'start': {
+                'dateTime': value.getDate(),
+                'timeZone': getTimezone(),
+            },
+            'end': {
+                'dateTime': new moment(value.getDate()).add(4, 'h').toDate(),
+                'timeZone': getTimezone(),
+            },
+        };
+        insertEvent(auth, googleCalendarEvent);
+    });
+}
 
-    MatchplanEntry.prototype.setDate = function(date) {
-        this.date = date;
-    };
-
-    MatchplanEntry.prototype.getLocation = function() {
-        return this.location;
-    };
-
-    MatchplanEntry.prototype.setLocation = function(location) {
-        this.location = location;
-    };
-
-    MatchplanEntry.prototype.getHomeTeam = function() {
-        return this.hometeam;
-    };
-
-    MatchplanEntry.prototype.setHomeTeam = function(hometeam) {
-        this.hometeam = hometeam;
-    };
-
-    MatchplanEntry.prototype.getVisitingTeam = function() {
-        return this.visitingteam;
-    };
-
-    MatchplanEntry.prototype.setVisitingTeam = function(visitingteam) {
-        this.visitingteam = visitingteam;
-    };
-
-    MatchplanEntry.prototype.getURL = function() {
-        return this.url;
-    };
-
-    MatchplanEntry.prototype.setURL = function(url) {
-        this.url = url;
-    };
-
-    MatchplanEntry.prototype.getDescription = function() {
-        return this.description;
-    };
-
-    MatchplanEntry.prototype.setDescription = function(description) {
-        this.description = description;
-    };
-
-    /**
-     * Get the match plan entry in string respresentation.
-     *
-     * e.g. "19.3.2017, 11:00:00: Victoria Osternburg vs GVO Oldenburg IV"
-     */
-    MatchplanEntry.prototype.toString = function() {
-        return this.date.toLocaleString() + ': ' + this.hometeam + ' vs ' + this.visitingteam;
-    };
-
-    return MatchplanEntry;
-})();
+/**
+ * Lists the next 10 events on the user's primary calendar.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ * @param {Object} event A Google calendar event.
+ */
+function insertEvent(auth, event) {
+    var calendar = google.calendar('v3');
+    calendar.events.insert({
+        auth: auth,
+        calendarId: '4ag87iqhp7nr3u67ncriuvn0js@group.calendar.google.com', // GVO4test
+        resource: event,
+    }, function (err, event) {
+        if (err) {
+            console.log('There was an error contacting the Calendar service: ' + err);
+            return;
+        }
+        console.log('Event created: %s', event.htmlLink);
+    });
+}
